e80864e2646495f4781d6f1bf4d6acb1aff3fc6e
Dain <kim@uni.minerva.edu>
2024-07-22 19:11:16 +0900

    Add jumping script

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class BirdScript : MonoBehaviour
 {
+    public Rigidbody2D myRigidbody;
+    public float flapStrength = 10;
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.name = "Bob Birdington";
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space) == true)
+        {
+            myRigidbody.velocity = Vector2.up * flapStrength;
+        }
         
     }
 }
